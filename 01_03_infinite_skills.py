fc785adb357fc86f316d2db938cfa854394d022c
Noel La Borde <138098592+noel-la-borde@users.noreply.github.com>
2024-08-29 09:21:40 -0400

    Add files via upload

---
+++ 01_03_infinite_skills.py
@@ -0,0 +1,5 @@
+# If I had infinite programming skills, I would create a code-based artificial brain to be implemented in robots to try to match or even surpass human inteligence. 
+
+# If I had infinite programming skills, I would create a medical tool to share the aproximate date of a person's death according to its health habits. 
+
+# If I had infinite programming skills, I would create a mind reader tool with chips that would send coded information to a computer to later be decoded. 
