bc90278ab0bd1d513fe8be2d3ea408e681c2abf3
Noel La Borde <138098592+noel-la-borde@users.noreply.github.com>
2024-08-29 09:20:48 -0400

    Add files via upload

---
+++ 01_01_python.org.py
@@ -0,0 +1,2 @@
+# The home page of https://python.org has a very ineteresting design to share all the information related to python as a programming language. 
+# The website contains very important tips and stuff that I'm not very familiar with yet, but I know are going to be useful as I get comfortable with the language. 
