eacdbbd7f89ffd2e8f48b19bc9b45189ccc4c46d
Noel La Borde <138098592+noel-la-borde@users.noreply.github.com>
2024-10-03 08:26:02 -0400

    Rename 01_02_hello_world.py to chapter_1/01_02_hello_world.py

--- 01_02_hello_world.py
+++
@@ -1,3 +0,0 @@
-print (Hello World!) 
-
-# the code is giving the error because it is missing the quotations ("") inside the parenthesis, to fix it, just add them to the code line.         
---
+++ chapter_1/01_02_hello_world.py
@@ -0,0 +1,3 @@
+print (Hello World!) 
+
+# the code is giving the error because it is missing the quotations ("") inside the parenthesis, to fix it, just add them to the code line.         
