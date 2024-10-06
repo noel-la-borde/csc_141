4c5da6901427e809475bc7aefa68ca4932ab3fc8
Noel La Borde <138098592+noel-la-borde@users.noreply.github.com>
2024-10-06 13:55:33 -0400

    Add files via upload

---
+++ 05_01_conditional_tests.py
@@ -0,0 +1,37 @@
+# Conditional Tests
+
+car= "Mazda"
+print ("Is my car a Mazda? I predict True.")
+print (car == 'Mazda')
+
+print ("\n Is my car a Honda? I predict False")
+print (car== "Honda")
+
+
+t_shirt= "Blue"
+print ("\nIs my t-shirt blue? I predict True.")
+print (t_shirt == "Blue")
+
+print ("\nIs my t-shirt white? I predict False")
+print (t_shirt== "White")
+
+age= "23"
+print ("\nAm I 23 years old? I predict True")
+print (age == "23")
+
+print ("\nAm I 25 years old? I predict False")
+print (age == "25")
+
+favorite_food= "Pasta"
+print ("\nIs pasta my favorite food? I predict True")
+print (favorite_food =='Pasta')
+
+print ("\nIs rice my favorite food? I predict False")
+print (favorite_food =='Rice')
+
+music_preference= "Pop"
+print ("\nIs pop my preferred music genre? I predict True")
+print (music_preference == "Pop")
+
+print ("\nIs Jazz my preferred music genre? I predict False")
+print (music_preference == "Jazz")
